0f65cd5f449fced48eb4e21115fd6a8270c76510
JoonKyo <joonkyolee@gmail.com>
2023-10-08 01:16:11 +0900

    feat(InvalidMessage): 로또 번호에 대한 오류 메시지 추가

--- src/main/java/lotto/domain/InvalidMessage.java
+++ src/main/java/lotto/domain/InvalidMessage.java
@@ -3,7 +3,12 @@ package lotto.domain;
 public enum InvalidMessage {
     PURCHASE_IS_NOT_NUMBER("구입 금액이 수가 아닙니다."),
     PURCHASE_IS_ZERO("0원보다 큰 금액을 입력해주세요."),
-    PURCHASE_IS_NOT_DIVIDED_BY_THOUSAND("금액은 1,000원 단위로 입력해주세요.");
+    PURCHASE_IS_NOT_DIVIDED_BY_THOUSAND("금액은 1,000원 단위로 입력해주세요."),
+    LOTTO_NUMBER_IS_NOT_NUMBER("입력한 로또 번호에 수가 아닌 값이 포함되어 있습니다."),
+    LOTTO_NUMBER_IS_NOT_IN_RANGE("1 ~ 45사이의 수를 입력해주세요."),
+    LOTTO_NUMBER_IS_DUPLICATED("서로 다른 수를 입력해주세요."),
+    LOTTO_NUMBER_LENGTH_IS_NOT_SIX("6개의 수를 입력해주세요."),
+    LOTTO_NUMBER_HAS_OTHER_SEPARATOR("구분자는 ,로 입력해주세요.");
 
     private static final String ERROR = "[ERROR] ";
     private final String errorMessage;
