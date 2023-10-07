7de5f2ad6c0db9f6123629da5dad55585eed0ade
JoonKyo <joonkyolee@gmail.com>
2023-10-08 02:18:22 +0900

    feat(Lotto): 로또 당첨 번호가 유효한지 확인하는 메서드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,8 +1,12 @@
-package lotto;
+package lotto.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int LOTTO_FIRST_NUMBER = 1;
+    private static final int LOTTO_LAST_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -10,11 +14,35 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
+
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (!hasSixNumbers(numbers)) {
+            throw new IllegalArgumentException(InvalidMessage.LOTTO_NUMBER_LENGTH_IS_NOT_SIX.getErrorMessage());
+        }
+        else if(!hasInRange(numbers)) {
+            throw new IllegalArgumentException(InvalidMessage.LOTTO_NUMBER_IS_NOT_IN_RANGE.getErrorMessage());
+        }
+        else if(hasDuplicatedNumber(numbers)){
+            throw new IllegalArgumentException(InvalidMessage.LOTTO_NUMBER_IS_DUPLICATED.getErrorMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasSixNumbers(List<Integer> numbers){
+        return numbers.size() == LOTTO_NUMBER_SIZE;
+    }
+
+    private boolean hasInRange(List<Integer> numbers){
+        return numbers.stream()
+                .filter(number -> LOTTO_FIRST_NUMBER < number && number < LOTTO_LAST_NUMBER)
+                .count() == LOTTO_NUMBER_SIZE;
+    }
+
+    private boolean hasDuplicatedNumber(List<Integer> numbers){
+        return numbers.stream()
+                .distinct()
+                .count() != LOTTO_NUMBER_SIZE;
+    }
 }
