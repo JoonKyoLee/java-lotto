796879223112168c5ad27a7d237b6ef3d09e9d6b
JoonKyo <joonkyolee@gmail.com>
2023-10-08 02:51:41 +0900

    feat(BonusNumber): 상수 수정

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,13 +1,13 @@
 package lotto.domain;
 
 public class BonusNumber {
-    private static final int BONUS_NUMBER_SIZE = 1;
     private static final int BONUS_FIRST_NUMBER = 1;
     private static final int BONUS_LAST_NUMBER = 45;
     private final int bonus;
+    private Lotto lotto;
 
-    public BonusNumber(int number, Lotto lotto){
-        validate(number, lotto);
+    public BonusNumber(int number){
+        validate(number);
         bonus = number;
     }
 
@@ -15,11 +15,11 @@ public class BonusNumber {
         return bonus;
     }
 
-    private void validate(int number, Lotto lotto){
+    private void validate(int number){
         if(!isInRange(number)){
             throw new IllegalArgumentException(InvalidMessage.BONUS_NUMBER_IS_NOT_IN_RANGE.getErrorMessage());
         }
-        else if(hasDuplicatedNumber(number, lotto)){
+        else if(hasDuplicatedNumber(number)){
             throw new IllegalArgumentException(InvalidMessage.BONUS_NUMBER_IS_NOT_UNIQUE.getErrorMessage());
         }
     }
@@ -28,7 +28,7 @@ public class BonusNumber {
         return BONUS_FIRST_NUMBER <= number && number <= BONUS_LAST_NUMBER;
     }
 
-    private boolean hasDuplicatedNumber(int number, Lotto lotto){
+    private boolean hasDuplicatedNumber(int number){
         return lotto.getNumbers().contains(number);
     }
 }
