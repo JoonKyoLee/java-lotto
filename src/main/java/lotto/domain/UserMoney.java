3730264939874fb1915c75fa3d356f952704aa88
JoonKyo <joonkyolee@gmail.com>
2023-10-08 00:23:59 +0900

    feat(UserMoney): 구매 금액이 유효한 값인지 확인하는 메서드 추가

--- src/main/java/lotto/domain/UserMoney.java
+++ src/main/java/lotto/domain/UserMoney.java
@@ -1,2 +1,40 @@
-package lotto.domain;public class UserMoney {
+package lotto.domain;
+
+public class UserMoney {
+    private static final int THOUSAND = 1000;
+    private static final int ZERO = 0;
+    private final int money;
+
+    public UserMoney(String moneyString){
+        money = convertMoney(moneyString);
+        checkException(moneyString);
+    }
+
+    private int convertMoney(String moneyString){
+        return Integer.parseInt(moneyString);
+    }
+
+    private void checkException(String moneyString){
+        if(!isNumber(moneyString)){
+            throw new IllegalArgumentException(InvalidMessage.PURCHASE_IS_NOT_NUMBER.getErrorMessage());
+        }
+        else if(isZero()){
+            throw new IllegalArgumentException(InvalidMessage.PURCHASE_IS_ZERO.getErrorMessage());
+        }
+        else if(!isDividedByThousand()){
+            throw new IllegalArgumentException(InvalidMessage.PURCHASE_IS_NOT_DIVIDED_BY_THOUSAND.getErrorMessage());
+        }
+    }
+
+    private boolean isNumber(String moneyString){
+        return moneyString.chars().allMatch(Character::isDigit);
+    }
+
+    private boolean isZero(){
+        return money == ZERO;
+    }
+
+    private boolean isDividedByThousand(){
+        return money % THOUSAND == ZERO;
+    }
 }
