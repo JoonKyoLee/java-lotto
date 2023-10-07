1c75fd7af4008452f833de8ffd3099b7d39a6d87
JoonKyo <joonkyolee@gmail.com>
2023-10-08 01:13:41 +0900

    feat(InputView): 구입 금액이 수인지 확인하는 메서드 추가 및 리턴 값을 정수형으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,14 +2,16 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import lotto.domain.InvalidMessage;
+
 public class InputView {
-    private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.\n";
+    private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.\n";
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.\n";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.\n";
 
-    public static String enterAmount(){
-        System.out.println(INPUT_AMOUNT_MESSAGE);
-        return Console.readLine();
+    public static int enterMoney(){
+        System.out.println(INPUT_MONEY_MESSAGE);
+        return convertMoney(Console.readLine());
     }
 
     public static String enterWinningNumber(){
@@ -20,5 +22,16 @@ public class InputView {
     public static String enterBonusNumber(){
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
+    }
+
+    private static int convertMoney(String money){
+        if(!isNumber(money)){
+            throw new IllegalArgumentException(InvalidMessage.PURCHASE_IS_NOT_NUMBER.getErrorMessage());
+        }
+        return Integer.parseInt(money);
+    }
+
+    private static boolean isNumber(String moneyString){
+        return moneyString.chars().allMatch(Character::isDigit);
     }
 }
