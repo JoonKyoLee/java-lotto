bcd391082111144866adc0a3bbfab49e088bcc90
JoonKyo <joonkyolee@gmail.com>
2023-10-08 01:15:06 +0900

    feat(UserMoney): 구입 금액이 수인지 확인하는 메서드 삭제

--- src/main/java/lotto/domain/UserMoney.java
+++ src/main/java/lotto/domain/UserMoney.java
@@ -7,9 +7,9 @@ public class UserMoney {
     private final int lottoGameNumber;
 
 
-    public UserMoney(String moneyString){
-        money = convertMoney(moneyString);
-        checkException(moneyString);
+    public UserMoney(int moneyInput){
+        money = moneyInput;
+        validate();
         lottoGameNumber = calculateLottoGameNumber();
     }
 
@@ -21,24 +21,13 @@ public class UserMoney {
         return lottoGameNumber;
     }
 
-    private int convertMoney(String moneyString){
-        return Integer.parseInt(moneyString);
-    }
-
-    private void checkException(String moneyString){
-        if(!isNumber(moneyString)){
-            throw new IllegalArgumentException(InvalidMessage.PURCHASE_IS_NOT_NUMBER.getErrorMessage());
-        }
-        else if(isZero()){
+    private void validate(){
+        if(isZero()){
             throw new IllegalArgumentException(InvalidMessage.PURCHASE_IS_ZERO.getErrorMessage());
         }
         else if(!isDividedByThousand()){
             throw new IllegalArgumentException(InvalidMessage.PURCHASE_IS_NOT_DIVIDED_BY_THOUSAND.getErrorMessage());
         }
-    }
-
-    private boolean isNumber(String moneyString){
-        return moneyString.chars().allMatch(Character::isDigit);
     }
 
     private boolean isZero(){
