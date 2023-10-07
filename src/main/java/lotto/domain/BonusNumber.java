11a73de60796fc1f32ce5e0c15363799b6f8dd56
JoonKyo <joonkyolee@gmail.com>
2023-10-08 02:30:53 +0900

    feat(BonusNumber): 보너스 번호에 대한 유효성을 검증하는 메서드 추가

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,2 +1,34 @@
-package lotto.domain;public class BonusNumber {
+package lotto.domain;
+
+public class BonusNumber {
+    private static final int BONUS_NUMBER_SIZE = 1;
+    private static final int BONUS_FIRST_NUMBER = 1;
+    private static final int BONUS_LAST_NUMBER = 45;
+    private final int bonus;
+
+    public BonusNumber(int number, Lotto lotto){
+        validate(number, lotto);
+        bonus = number;
+    }
+
+    public int getBonusNumber(){
+        return bonus;
+    }
+
+    private void validate(int number, Lotto lotto){
+        if(!isInRange(number)){
+            throw new IllegalArgumentException(InvalidMessage.BONUS_NUMBER_IS_NOT_IN_RANGE.getErrorMessage());
+        }
+        else if(hasDuplicatedNumber(number, lotto)){
+            throw new IllegalArgumentException(InvalidMessage.BONUS_NUMBER_IS_NOT_UNIQUE.getErrorMessage());
+        }
+    }
+
+    private boolean isInRange(int number){
+        return BONUS_FIRST_NUMBER <= number && number <= BONUS_LAST_NUMBER;
+    }
+
+    private boolean hasDuplicatedNumber(int number, Lotto lotto){
+        return lotto.getNumbers().contains(number);
+    }
 }
